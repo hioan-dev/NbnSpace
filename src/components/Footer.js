18928d0baaf84b572c070e4e9ea32d072646ee99
Mr.halomoan n <oansmaturapax123@gmail.com>
2022-04-22 13:11:14 +0700

    update Components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 function Footer(){
     return (
-        <footer className="flex text-center px-4 py-8 justify-center">
+    <footer className="flex text-center px-4 py-8 justify-center">
       <p className="text-sm">
         Copyright 2022 • All Rights Reserved NbnSpace by Halomoan Nababan
       </p>
     </footer>
-    )
+  )
 }
 
 export default Footer;
